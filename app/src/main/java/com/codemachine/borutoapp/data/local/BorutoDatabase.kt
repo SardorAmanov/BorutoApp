a75ab94ef727974d00c93b101723d74f5f1973ed
Sardor Amanov <sardor.amanov.1998@gmail.com>
2023-05-25 15:11:51 +0500

    room database setup

---
+++ app/src/main/java/com/codemachine/borutoapp/data/local/BorutoDatabase.kt
@@ -0,0 +1,16 @@
+package com.codemachine.borutoapp.data.local
+
+import androidx.room.Database
+import androidx.room.TypeConverters
+import com.codemachine.borutoapp.data.local.dao.HeroDao
+import com.codemachine.borutoapp.data.local.dao.HeroRemoteKeyDao
+import com.codemachine.borutoapp.domain.model.Hero
+import com.codemachine.borutoapp.domain.model.HeroRemoteKey
+
+@Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
+abstract class BorutoDatabase {
+
+    abstract fun heroDao(): HeroDao
+    abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
+}
