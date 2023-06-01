ae643469fc0e57089692db2ac29b865e0af9bd42
Sardor Amanov <sardor.amanov.1998@gmail.com>
2023-06-01 16:16:16 +0500

    SplashScreen

--- app/src/main/java/com/codemachine/borutoapp/MainActivity.kt
+++ app/src/main/java/com/codemachine/borutoapp/MainActivity.kt
@@ -10,39 +10,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.codemachine.borutoapp.navigation.SetupNavGraph
 import com.codemachine.borutoapp.ui.theme.BorutoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BorutoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BorutoAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/codemachine/borutoapp/presentation/screens/splash/SplashScreen.kt
@@ -0,0 +1,58 @@
+package com.codemachine.borutoapp.presentation.screens.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import com.codemachine.borutoapp.R
+import com.codemachine.borutoapp.ui.theme.Purple40
+import com.codemachine.borutoapp.ui.theme.Purple500
+import com.codemachine.borutoapp.ui.theme.Purple700
+
+@Composable
+fun SplashScreen(navController: NavHostController) {
+    Splash()
+}
+
+@Composable
+fun Splash() {
+    if (isSystemInDarkTheme()) {
+        Box(modifier = Modifier
+            .background(Color.Black)
+            .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(R.string.app_logo)
+            )
+        }
+    } else {
+        Box(modifier = Modifier
+            .background(Brush.verticalGradient(listOf(Purple700, Purple500)))
+            .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = stringResource(R.string.app_logo)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SplashScreenPreview() {
+    Splash()
+}
